ea8be239d93549d95859cda1333517ea5bbe1699
Rily <rilumph@yahoo.co.jp>
2017-04-22 01:31:30 +0900

    [ADD] ls command alias
    ls: Add default options
    - color
    - show identity icon (dir:/, link:@, exe:*, etc...)
    - sort extension and directory first
    
    la: extends optional ls
    - show all file except backup file and . or ..
    
    ll: extend optional la
    - clear terminal before exe
    - list view
    - human readable
    - set date time format

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -7,3 +7,14 @@ alias rm='rm -Iv --one-file-system'
 alias mv='mv -bv --suffix=".bak"'
 alias cp='cp -bv --suffix=".bak"'
 
+# Judge to add color
+if [[ -x /usr/bin/dircolors ]]; then
+  alias ls='ls -v --color=auto --classify --format=across --group-directories-first --sort=extension'
+  alias la='ls --almost-all --ignore-backups'
+  alias ll='clear && la -l --color=auto --human-readable --time-style="+%y-%m-%d %H:%M:%S"'
+else
+  alias ls='ls -v --classify --format=across --group-directories-first --sort=extension'
+  alias la='ls --almost-all --ignore-backups'
+  alias ll='clear && la -l --color=auto --human-readable --time-style="+%y-%m-%d %H:%M:%S"'
+fi
+
