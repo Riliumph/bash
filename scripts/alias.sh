32fa35c8e0323d99466aefd4fbc50fab0a88ad7f
Rily <rilumph@yahoo.co.jp>
2017-04-22 01:27:02 +0900

    [ADD] File operation alias
    rm
    - Don't remove other file sysmtem for safety
    - Show progress
    
    mv / cp
    - Back up to avoid overwritting destination
    - Show progress

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,3 +2,8 @@
 alias refresh='source $HOME/.bashrc && echo "Refresh Bash"'
 alias version='echo "bash versions: ${BASH_VERSION}"'
 
+# File & Directory operation
+alias rm='rm -Iv --one-file-system'
+alias mv='mv -bv --suffix=".bak"'
+alias cp='cp -bv --suffix=".bak"'
+
