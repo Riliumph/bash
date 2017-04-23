0b9e5fffddee09a01373e013e57089cd5648c42b
Rily <rilumph@yahoo.co.jp>
2017-04-24 03:30:05 +0900

    [ADD] bash_history setting
    Commonize bash_history at each terminal
    Changed not to memorize commands of 4 or fewer characters
    Changed not to memorize cd, which and history command

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,3 +3,23 @@
 #export LC_ALL='ja_JP.UTF-8'
 #export LC_MESSAGES='ja_JP.UTF-8'
 
+########## HISTORY SETTING ##########
+share_history()
+{
+  history -a  # Add a previous command to bash_history
+  history -c  # Clear local history in terminal
+  history -r  # Reload history from bash_history
+}
+
+PROMPT_COMMAND='share_history'
+shopt -u histappend  # Unset shell option
+export HISTSIZE=9999
+export HISTCONTROL=ignoreboth   # ex) ignoredups,ignorespace,erasedups
+### Ignore commands
+# command name with 4 or fewer characters
+export HISTIGNORE=?:??:???:????:${HISTIGNORE}
+# Specific command names
+export HISTIGNORE=cd*:${HISTIGNORE}
+export HISTIGNORE=which*:${HISTIGNORE}
+export HISTIGNORE=history:${HISTIGNORE}
+
