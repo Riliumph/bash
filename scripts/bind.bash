09b3b0982cc4494faf1b619ede2ae945a2bd8f60
Riliumph <rilumph@yahoo.co.jp>
2017-08-12 00:23:35 +0900

    [ADD] key bind of move parent directory
    Add Alt+UP key combination as moving parent directory key

--- scripts/bind.bash
+++ scripts/bind.bash
@@ -1,8 +1,10 @@
 # Ctrl+Up-Arrow : Move parent directory
 if [[ ${PS1} ]]; then
-	# \e[1;5A : Ctrl-Up
-	# \er : redraw PS1 @ GNU readline
-	bind '"\e[1;5A":"\ercd ../\n"'
-	# Attention: No need bind's option "-x"
+  # \er : redraw PS1 @ GNU readline
+  # \e[1;5A : Ctrl-Up
+  bind '"\e[1;5A":"\ercd ../\n"'
+  # \e[1;3A : Alt-Up
+  bind '"\e[1;3A":"\ercd ../\n"'
+  # Attention: No need bind's option "-x"
 fi
 
