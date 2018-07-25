394d8d64ecfe516b91d363b57b013cdc1a98b17f
Riliumph <riliumph@gmail.com>
2018-07-26 01:25:13 +0900

    [Update] move directory command
    Refine so that it can be executed even when characters are inputed

--- scripts/bind.bash
+++ scripts/bind.bash
@@ -3,9 +3,9 @@
 if [[ ${PS1} ]]; then
   # \er : redraw PS1 @ GNU readline
   # \e[1;5A : Ctrl-Up
-  bind '"\e[1;5A":"\ercd ../\n"'
+  bind '"\e[1;5A":"\e[3;5~\ercd ../\n"'
   # \e[1;3A : Alt-Up
-  bind '"\e[1;3A":"\ercd ../\n"'
+  bind '"\e[1;3A":"\e[3;5~\ercd ../\n"'
 fi
 
 # Alt+Down: Select the destination
