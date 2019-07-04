4f8277826418dc399cf6bed7dff66c679590b821
Riliumph <riliumph@gmail.com>
2019-07-05 00:11:03 +0900

    [add] judging os logic

--- scripts/env.bash
+++ scripts/env.bash
@@ -1,3 +1,15 @@
+########## OPERATION SYSTEM ##########
+if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  OS='Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+  OS='Cygwin'
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
+
 ########## ENVIRONMNT LANGUAGE ##########
 #export LANG='ja_JP.UTF-8'
 #export LC_ALL='ja_JP.UTF-8'
