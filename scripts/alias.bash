cec3d0bf4ddf281c23746db02f0a2275afb98797
riliumph <riliumph@gmail.com>
2020-06-24 23:55:31 +0900

    [Fix] forget staging

--- scripts/alias.bash
+++ scripts/alias.bash
@@ -1,5 +1,5 @@
 if [[ ${OS} = "Linux" ]]; then
-  source $BASH_ROOT/scripts/alias/for_linux.bash
+  source $BASH_ROOT/scripts/alias/linux.bash
 elif [[ ${OS} = "Mac" ]]; then
-  source $BASH_ROOT/scripts/alias/for_macos.bash
+  source $BASH_ROOT/scripts/alias/macos.bash
 fi
