d0b2bdc5f34ed8ba230f62d29f8ae51d48731d6c
riliumph <riliumph@gmail.com>
2020-11-11 23:17:13 +0900

    [Update] PS1
    Use PROMPT_COMMAND instead of executing command in PS1
    Fix the leak of escape sequence before and after tput
    Define the variable of status code as global variable

--- scripts/prompt/macos.bash
+++ scripts/prompt/macos.bash
@@ -16,54 +16,46 @@ if which git &> /dev/null; then
   export GIT_PS1_SHOWDIRTYSTATE=1
 fi
 
-
 ###
-# StatusFace
-# Judge last command status
-# Don't use MakeControlSequence
-# This function should be executed every paint
-# So, don't use escape sequence as string
-StatusFace() {
-  local status=$?
-  if [ $status -eq 0 ]; then
-    color=$(tput setaf 2)
-    face="(*'_')< "
-  else
-    color=$(tput setaf 1)
-    face="(*;_;)< "
-  fi
-  face+="$color""$status"
-  echo "${face}"
-}
-
-###
-# GetPromptString
-# Get prompt string in terminal
-SetPromptString()
-{
+# Dispatch
+# Do not write complex and long command in PROMPT_COMMAND directly
+# Use the dispatch function that wraps the process
+Dispatch(){
+  export EXIT_STATUS="$?"
+  local last_status=${EXIT_STATUS}
+  # Should enclose tput by escape sequence according to man page
+  local norm="\[$(tput sgr0)\]"
+  ps1=""
+  # Factory of Line 1
   local GIT_BRANCH=''
   if which git &> /dev/null; then
-    GIT_BRANCH='$(__git_ps1)' # Use single-quotation for dynamic prompt
+    # Use single-quotation to input function name in PS1
+    # __git_ps1 function need executing when PS1 is interpreted.
+    GIT_BRANCH='$(__git_ps1)'
   fi
-  local norm=$(tput sgr0)
-  local user=$(tput setaf 4)
-  local path=$(tput setaf 3)
-  local git=$(tput setaf 1)
-  ps1=""
-  ps1+="$user\u"
-  ps1+="$norm:"
-  ps1+="$path\w"
-  ps1+="$norm|"
-  ps1+="$git""$GIT_BRANCH"
-  ps1+="$norm\n"
-  ps1+='$(StatusFace)' # Use single-quotation for dynamic prompt
-  ps1+="$norm \$"
-  echo $ps1
+  local user="\[$(tput setaf 4)\]"
+  local path="\[$(tput setaf 3)\]"
+  local git="\[$(tput setaf 1)\]"
+  ps1+="${user}\u"
+  ps1+="${norm}:"
+  ps1+="${path}\w"
+  ps1+="${norm}|"
+  ps1+="${git}${GIT_BRANCH}"
+  ps1+="${norm}\n"
+  # Factory of Line 2
+  if [ ${last_status} -eq 0 ]; then
+    face="(*'_')< "
+    err="\[$(tput setaf 2)\]"
+  else
+    face="(*;_;)< "
+    err="\[$(tput setaf 1)\]"
+  fi
+  ps1+="${face}"
+  ps1+="${err}${last_status} "
+  ps1+="${norm}\$ "
+  # Finalize PS1
+  PS1=${ps1}
 }
-# At this timing, dynamic prompt function need executing.
-# Dynamic prompt function:
-# - __git_ps1
-# - StatusFace
-PS1="$(SetPromptString) "
+export PROMPT_COMMAND='Dispatch'
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME:+$FUNCNAME(): }'
 
