7c0d3a84d232ca0cdfa6b5b4f8b899590178332e
riliumph <riliumph@gmail.com>
2021-09-05 17:46:00 +0900

    [Fix] fix typo

--- conf.d/global.bash
+++ conf.d/global.bash
@@ -1,4 +1,4 @@
 ### global variable tools
 export LAST_STATUS=0 # Initialize status code by 0
 # Attention to how to escape single-quotation
-unique='awk '\''!dictionaty[$0]++'\''' # Need not sort -f unlike uniq command
+unique='awk '\''!dictionary[$0]++'\''' # Need not sort -f unlike uniq command
