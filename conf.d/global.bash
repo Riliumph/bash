f9ee7d129676913a29d915f4d81b97d785efdbdd
riliumph <riliumph@gmail.com>
2021-09-29 09:58:55 +0900

    [Update] change environment variable to shell variable
    Go language's library viper cannot read environment varable crrectly
    If CACHE exists, viper cannot read HOGE_CACHE_NAME

--- conf.d/global.bash
+++ conf.d/global.bash
@@ -1,12 +1,12 @@
 ### global variable tools
-export LAST_STATUS=0 # Initialize status code by 0
+LAST_STATUS=0 # Initialize status code by 0
 # Attention to how to escape single-quotation
 unique='awk '\''!dictionary[$0]++'\''' # Need not sort -f unlike uniq command
 
-# Check environment value
+# Exsit shell variable
 if [[ ! -v CACHE ]]; then
   # If env variable is undefined, set default path,
-  export CACHE=${BASH_ROOT}/cache
+  CACHE=${BASH_ROOT}/cache
 fi
 
 # Check log directory existance
