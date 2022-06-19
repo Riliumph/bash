f041ef921d73888fa8a753c043a554a8f9bfc6d7
riliumph <riliumph@gmail.com>
2022-06-20 01:43:38 +0900

    [Update] reformat

--- conf.d/completion.bash
+++ conf.d/completion.bash
@@ -13,8 +13,10 @@ fi
 # ex)
 # complete -F _comp_only_file less
 # @param command name
-_comp_only_file(){
+_comp_only_file()
+{
   COMPREPLY=( `compgen -W "$(ls -Fv | grep -v /@*)" $2` )
 }
 
 complete -d ls  # default: complete -F _longopt ls
+
--- functions/prompt.bash
+++ functions/prompt.bash
@@ -45,12 +45,3 @@ PromptFactory()
   # Finalize PS1
   echo "${ps1}"
 }
-
-###
-# Dispatch
-# Do not write complex and long command in PROMPT_COMMAND directly
-# Use the dispatch function that wraps the process
-Dispatch(){
-  export LAST_STATUS="$?"
-  share_history
-}
