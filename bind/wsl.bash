83d97d3ab531717195d1a5898cda27abfade4c8f
riliumph <riliumph@gmail.com>
2022-06-25 23:07:41 +0900

    [Fix] cannot read symbolic link in wsl

--- bind/wsl.bash
+++ bind/wsl.bash
@@ -1,1 +1,9 @@
-linux.bash
+# Attention: No need bind's option "-x"
+if [[ ${PS1} ]]; then
+  bind '"\e[1;3A":"\e[3;5~\ercd ../\n"'
+fi
+
+if type peco &> /dev/null; then
+  bind '"\e[1;3B":"\ercd\n"'
+fi
+
