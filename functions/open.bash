428e28c8fad7bab1cfa7bea4f8a3af2d5ef9daf7
riliumph <riliumph@gmail.com>
2023-01-09 13:21:29 +0900

    [update] support open func like mac in wsl2

--- functions/open.bash
+++ functions/open.bash
@@ -1,4 +1,26 @@
+###
+# open like mac
+# alias open=explorer.exeには二つ問題がある
+# - デスクトップ上に同名ファイルが存在する場合、そちらを開いてしまう
+#   - open hoge.jpg など動かないケース有り
+# - カレントディレクトリ以外に開けない
+#   - open ../hoge.jpg などは解釈できない
+#
+if ! type wslpath &> /dev/null; then
+  return ;
+fi
+
 open()
 {
-    cmd.exe /c start .
+  if [ $# != 1 ]; then
+    echo "Usage: open [PATH]"
+    echo "Example:"
+    echo "  open ."
+    return 1;
+  fi
+  if [ ! -e $1 ]; then
+    echo "open: $1 : No such file or directory"
+    return 1;
+  fi
+  cmd.exe /c start "$(wslpath -w "$1")" 2> /dev/null
 }
