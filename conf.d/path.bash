68127689642fbbd3f98c1128a15bd29c0ef98e97
Riliumph <riliumph@outlook.jp>
2023-04-20 03:27:22 +0900

    [fix] remove duplicate paths in the PATH
    Replace incorrect separator with correct character
    Add logic to remove trailing separator

--- conf.d/path.bash
+++ conf.d/path.bash
@@ -1,5 +1,5 @@
 # Set up special command path
-path="$BASH_ROOT/.local/bin":"$PATH"
+path="$BASH_ROOT/.local/bin:$PATH"
 # remove path duplicatipn
-path=$(echo "${path}" | tr ";" "\\n" | unique | tr "\\n" ";")
-PATH=${path}
+path=$(echo -n "${path}" | tr ":" "\\n" | unique | tr "\\n" ":")
+PATH="${path%:}"
