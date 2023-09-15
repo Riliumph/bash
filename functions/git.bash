847f3ae6b21710ce72a90b7de6dbe745bb6381e7
Riliumph <riliumph@outlook.jp>
2023-09-16 06:38:34 +0900

    [add] install function for git files
    install function for git-prompt
    install function for git-completion

--- functions/git.bash
+++ functions/git.bash
@@ -1,4 +1,7 @@
-GIT_LOCAL="./local/git"
+GIT_LOCAL="$BASH_ROOT/.local/git"
+GIT_FILE_LOCATION=("/usr/share")              # Default
+GIT_FILE_LOCATION+=("/usr/local/opt")         # for macOS
+GIT_FILE_LOCATION+=("/etc/bash_completion.d") # for ubuntu
 
 LoadGitFiles()
 {
@@ -9,5 +12,33 @@ LoadGitFiles()
   fi
   if [ -e "${GIT_LOCAL}/git-completion.bash" ]; then
     source "${GIT_LOCAL}/git-completion.bash"
+
+InstallGitPrompt()
+{
+  mkdir -p "${GIT_LOCAL}"
+  # Search git prompt
+  mapfile -t gitprompts < <(find "${GIT_FILE_LOCATION[@]}" -type f -name "git-prompt*" 2> /dev/null)
+  if (("${#gitprompts[@]}" == 0)); then
+    # Download mode
+    local -r from="https://raw.githubusercontent.com/git/git/master/contrib/completion"
+    curl -o "${GIT_LOCAL}/git-prompt.sh" "${from}/git-prompt.sh"
+  else
+    # local copy mode
+    cp -f "${gitprompts[0]}" "${GIT_LOCAL}/git-prompt.sh"
+  fi
+}
+
+InstallGitCompletion()
+{
+  mkdir -p "${GIT_LOCAL}"
+  # Search git prompt
+  mapfile -t gitprompts < <(find "${GIT_FILE_LOCATION[@]}" -type f -name "git-completion.bash" 2> /dev/null)
+  if (("${#gitprompts[@]}" == 0)); then
+    # Download mode
+    local -r from="https://raw.githubusercontent.com/git/git/master/contrib/completion"
+    curl -o "${GIT_LOCAL}/git-completion.bash" "${from}/git-completion.bash" 2> /dev/null
+  else
+    # local copy mode
+    cp -f "${gitprompts[0]}" "${GIT_LOCAL}/git-completion.bash"
   fi
 }
