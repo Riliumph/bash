d287b9179d5f18a03af03e7ad1d12d58726de368
Riliumph <riliumph@gmail.com>
2026-03-10 16:51:06 +0900

    [update] enhanced unique function
    deprecate eval command
    support stream process

--- functions/unique.bash
+++ functions/unique.bash
@@ -1,16 +1,13 @@
 unique()
 {
   local filepath="$1"
-  local lines
   # Check Pipeline
   if [ -p /dev/stdin ]; then
     filepath="/dev/stdin"
   fi
-  # -t: remove LF
-  mapfile -t lines < "$filepath"
   # main process
   # Attention to how to escape single-quotation
-  uniq='awk '\''!dictionary[$0]++'\'
+  local -r AWK_DEDUP='!dictionary[$0]++'
   # Need not sort -f unlike uniq command
-  printf "%s\n" "${lines[@]}" | eval "${uniq}"
+  awk "$AWK_DEDUP" "$filepath"
 }
