247a6bd2cb1746b6365bd81039c5168a298460bd
Riliumph <riliumph@gmail.com>
2026-03-10 17:22:13 +0900

    [add] no arg case in unique command

--- functions/unique.bash
+++ functions/unique.bash
@@ -3,10 +3,18 @@ unique()
   local filepath="$1"
   # Check Pipeline
   if [ -p /dev/stdin ]; then
+    # e.g. echo xxx | unique
     filepath="/dev/stdin"
   fi
-  # main process
-  # Attention to how to escape single-quotation
+
+  if [ -z "$filepath" ]; then
+    # e.g. unique / unique < xxx.txt
+    if [ -t 0 ]; then
+      # Attention: awk keeps reading input until it reaches EOF (stdin is closed).
+      echo "unique: waiting your input... exit: Ctrl-D" >&2
+    fi
+    filepath="/dev/stdin"
+  fi
   local -r AWK_DEDUP='!dictionary[$0]++'
   # Need not sort -f unlike uniq command
   awk "$AWK_DEDUP" "$filepath"
