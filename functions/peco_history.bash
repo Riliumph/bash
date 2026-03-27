3edc9691d4cb1ae93321a9853cb26fc92a7faef3
Riliumph <riliumph@gmail.com>
2026-03-27 19:07:58 +0900

    [update] simplify peco_history
    use fix command(fc) instead of history command

--- functions/peco_history.bash
+++ functions/peco_history.bash
@@ -8,23 +8,11 @@
 # 4.Pipe to peco
 PecoHistory()
 {
-  local sed_flag
-  local trim_line_number
-
-  case "${PF}" in
-    MacOS) sed_flag='-E' ;;
-    *)     sed_flag='-r' ;;
-  esac
-  trim_line_number='s/^[[:space:]]*[0-9]+[[:space:]]+//'
-  local -r CMD=$(\history \
+  local -r CMD=$(fc -ln \
     | reverse \
-    | sed "${sed_flag}" "${trim_line_number}" \
     | unique \
     | peco --query "${READLINE_LINE}")
   READLINE_LINE=${CMD}   # Input to terminal's readline
   READLINE_POINT=${#CMD} # Set cursor
 }
 
-if command -v peco &> /dev/null; then
-  bind -x '"\C-r": PecoHistory'
-fi
