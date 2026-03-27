7f945bb974e3ec988c298c617f4d5b1c58189013
Riliumph <riliumph@gmail.com>
2026-03-27 15:39:17 +0900

    [fix] typo

--- conf.d/path.bash
+++ conf.d/path.bash
@@ -1,5 +1,5 @@
 # Set up special command path
 PATH="$BASH_ROOT/.local/bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
-# remove path duplicatipn until now
+# remove path duplication until now
 PATH="$(path_unique)"
