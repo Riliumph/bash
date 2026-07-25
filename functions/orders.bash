cb6d76b3533da536415d2cf3f62a796045e74818
Riliumph <riliumph@outlook.jp>
2026-07-25 22:56:19 +0000

    refactor: simplify orders method arrangement

--- functions/orders.bash
+++ functions/orders.bash
@@ -1,3 +1,27 @@
+asc()
+{
+  local filepath="$1"
+  # Check Pipeline
+  if [ -p /dev/stdin ]; then
+    filepath="/dev/stdin"
+  fi
+
+  if [ -z "$filepath" ]; then
+    if [ -t 0 ]; then
+      echo "asc: waiting your input... exit: Ctrl-D" >&2
+    fi
+    filepath="/dev/stdin"
+  fi
+
+  # Check tool
+  if ! command -v sort &> /dev/null; then
+    echo "asc: sort command not found" >&2
+    return 1
+  fi
+
+  sort -f < "$filepath"
+}
+
 reverse()
 {
   local filepath="$1"
